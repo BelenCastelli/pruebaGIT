97526f981af12384e2ebfd82199afe08d5fff3f6
Belen Castelli <mariabelencastelli29@gmail.com>
2023-10-24 11:07:09 +0200

    nuevos archivos con funciones

---
+++ runFunciones.js
@@ -0,0 +1,9 @@
+let myLib = require("./funciones")
+
+
+// Llamar a todas las funciones de la librería e imprimir sus resultados.
+
+console.log(myLib.suma(10,5));
+console.log(myLib.resta(10,5));
+console.log(myLib.multiplicacion(10,5));
+console.log(myLib.division(10,5));
